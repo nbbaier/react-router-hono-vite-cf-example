3f9d8a744b4f9fcf742003f8777012d4fe450594
Nico Baier <nico.baier@gmail.com>
2025-04-27 21:29:35 -0500

    random changhes

--- workers/app.ts
+++ workers/app.ts
@@ -5,15 +5,15 @@ const app = new Hono();
 
 // Add more routes here
 
-app.get("*", c => {
-  const requestHandler = createRequestHandler(
-    () => import("virtual:react-router/server-build"),
-    import.meta.env.MODE
-  );
+app.get("*", (c) => {
+	const requestHandler = createRequestHandler(
+		() => import("virtual:react-router/server-build"),
+		import.meta.env.MODE,
+	);
 
-  return requestHandler(c.req.raw, {
-    cloudflare: { env: c.env, ctx: c.executionCtx }
-  })
-})
+	return requestHandler(c.req.raw, {
+		cloudflare: { env: c.env, ctx: c.executionCtx },
+	});
+});
 
 export default app;
